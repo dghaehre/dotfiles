c35fcb8a2f3a71806d86045f05dc0971c8943346
Daniel <dghaehre@gmail.com>
2022-05-29 12:29:47 +0200

    Merge branch 'master' of github.com:dghaehre/dotfiles
    * 'master' of github.com:dghaehre/dotfiles:
      update sync func

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -176,16 +176,16 @@ function fish_mode_prompt
   switch $fish_bind_mode
     case default
       set_color --bold yellow
-      echo -n "["(task _get rc.context)"] "
+      echo -n "["(date "+%H:%M")"] "
     case insert
       set_color brblack
-      echo -n "["(task _get rc.context)"] "
+      echo -n "["(date "+%H:%M")"] "
     case replace_one
       set_color --bold blue
-      echo -n "["(task _get rc.context)"] "
+      echo -n "["(date "+%H:%M")"] "
     case visual
       set_color --bold brmagenta
-      echo -n "["(task _get rc.context)"] "
+      echo -n "["(date "+%H:%M")"] "
     case '*'
       set_color --bold red
       echo '? '
