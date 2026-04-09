ab86797acebc8647fed8970e3946359857d1c4a6
Daniel <dghaehre@gmail.com>
2026-04-09 09:39:53 +0200

    comment out what is not in use

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,7 +14,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-neotest/nvim-nio" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- currently not in use..
+	-- { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- currently not in use..
 	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope-dap.nvim" },
 
